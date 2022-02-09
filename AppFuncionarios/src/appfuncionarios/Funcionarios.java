2860f5567ce12a946cec720fcfab256e56358def
Paulo Roberto J. Rodrigues <paulo-rjr11@hotmail.com>
2022-02-09 16:47:38 -0300

    Removendo alguns comentario no código da Classe Funcionarios.

--- AppFuncionarios/src/appfuncionarios/Funcionarios.java
+++ AppFuncionarios/src/appfuncionarios/Funcionarios.java
@@ -21,20 +21,8 @@ public class Funcionarios {
         this.funcao = funcao;
         this.falta = falta;
         this.salario = salario;
-    }
-    //Cosntrutor com Sobrecarga.
-    /*public Funcionarios(String nome, String funcao, int idade, double 
-            salario){
-        this.nome = nome;
-        this.idade = idade;
-        this.funcao = funcao;        
-        this.salario = salario;
-    }*/
-    //Método público do tipo double, sem parâmetro e se retorno.
-    /*Para calcular as faltas segui seguinte fórmula:
-    salario = ((salario / horas trabalhadas por mês) * horas trabalhadas por 
-    dia) * número de faltas)*/
-    //Método público com retorno para calcuar os descontos no salário.
+    }    
+    //Método público com retorno para calcular os descontos de imposto no salário.
     public double descontar(){
         if(this.idade >= 55 && this.falta == 0){            
             this.salario = this.salario - (this.salario * 0.09);  
@@ -44,28 +32,20 @@ public class Funcionarios {
         if(this.idade >= 55 && this.falta > 0){            
             this.salario = (this.salario - (this.salario * 0.09)) - 
             (((this.salario / 240) * 9) * this.falta);
-            System.out.println("\nImposto de 9%.");
-            
-        }else if(this.idade < 55 && this.falta > 0){            
+            System.out.println("\nImposto de 9%.");            
+        }
+		
+		else if(this.idade < 55 && this.falta > 0){            
             this.salario = (this.salario - (this.salario * 0.11)) - 
             (((this.salario / 240) * 9) * this.falta);
             System.out.println("\nImposto de 11%.");
-        }else if (this.idade < 55 && this.falta == 0){            
+			
+        }
+		
+		else if (this.idade < 55 && this.falta == 0){            
             this.salario = (this.salario - (this.salario * 0.11));
             System.out.println("\nImposto de 11%.");
-        }
-        /*if(idade >= 55){
-            salario = salario - (salario * 0.09);
-        }
-        if(idade >= 55 && falta > 0){
-            salario = (salario - (salario * 0.09)) - 
-                    (((salario / 240) * 9) * falta);
-        }else if(idade < 55 && falta > 0){
-            salario = (salario - (salario * 0.11)) - 
-                    (((salario / 240) * 9) * falta);
-        }else{
-             salario = (salario - (salario * 0.11));
-        }*/
+        }        
         return salario;
     }    
 }
